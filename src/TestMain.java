3896e8cbacabb0aab930cf18f45ee9b7e7152e22
wlchs <laci.dj@gmail.com>
2016-02-18 01:50:06 +0100

    Initial commit

---
+++ src/TestMain.java
@@ -0,0 +1,21 @@
+
+/**
+ * Created by Laszlo on 2016. 02. 18..
+ */
+
+/**
+ * Template comment describing the goal of the method.
+ * Test function for educational purposes to commence and teach the usage of bitbucket.
+ *
+ * Input:
+ * args contains the supplied command-line arguments as an array of String objects.
+ *
+ * Output:
+ * void type methods have no return value.
+ */
+public class TestMain {
+    public static void main(String[] args) {
+        // Anything which isn't evident at first glimpse, must be rigorously described in a comment beforehand.
+        System.out.println("Test method:" + System.lineSeparator() + "Starting one hell of a game from scratch!");
+    }
+}
